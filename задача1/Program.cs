54d9ad8774a3230b39f391bab3ce25a0384ccd00
Nadezda <nadenka.merkulowa@yandex.ru>
2023-04-26 12:05:21 +0500

    задача1

---
+++ задача1/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 3
+//Решние:
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+
+int[] InputArray(int Length)
+{
+    int[] array = new int[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Promt($"Введите {i + 1} первый элемент");
+
+
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}]={array[i]}");
+    }
+}
+
+int CountPoloschitNumber(int[] array)
+{
+
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int Length = Promt("Введите количество чисел>");
+int[] array;
+array = InputArray(Length);
+PrintArray(array);
+Console.WriteLine($"Количетсво чисел больше 0 - {CountPoloschitNumber(array)}");
+
+
